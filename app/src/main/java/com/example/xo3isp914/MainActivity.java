a03c739a4f7d3a201240368b04b893305b169a42
Студент <student7@iMac7.local>
2022-11-03 12:14:54 +0300

    first commit

--- app/src/main/java/com/example/xo3isp914/MainActivity.java
+++ app/src/main/java/com/example/xo3isp914/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.xo3isp914;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
     Button r3c2;
     Button r3c3;
     Button start;
+    TextView result;
+
+    Boolean XO = true;
+    Boolean isStarted = false;
+    Boolean restart = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,54 +32,91 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         r1c1 = (Button) findViewById(R.id.r1c1);
-        r1c1.setEnabled(false);
         r1c2 = (Button) findViewById(R.id.r1c2);
-        r1c2.setEnabled(false);
         r1c3 = (Button) findViewById(R.id.r1c3);
-        r1c3.setEnabled(false);
         r2c1 = (Button) findViewById(R.id.r2c1);
-        r2c1.setEnabled(false);
         r2c2 = (Button) findViewById(R.id.r2c2);
-        r2c2.setEnabled(false);
         r2c3 = (Button) findViewById(R.id.r2c3);
-        r2c3.setEnabled(false);
         r3c1 = (Button) findViewById(R.id.r3c1);
-        r3c1.setEnabled(false);
         r3c2 = (Button) findViewById(R.id.r3c2);
-        r3c2.setEnabled(false);
         r3c3 = (Button) findViewById(R.id.r3c3);
-        r3c3.setEnabled(false);
         start = (Button) findViewById(R.id.start);
+        result = (TextView) findViewById(R.id.Result);
+        start.setOnClickListener(this::onClickStart);
+        r1c1.setOnClickListener(this::onClick);
+        r1c2.setOnClickListener(this::onClick);
+        r1c3.setOnClickListener(this::onClick);
+        r2c1.setOnClickListener(this::onClick);
+        r2c2.setOnClickListener(this::onClick);
+        r2c3.setOnClickListener(this::onClick);
+        r3c3.setOnClickListener(this::onClick);
+        r3c2.setOnClickListener(this::onClick);
+        r3c1.setOnClickListener(this::onClick);
 
-        Button[][] massBtn = new Button[3][3];
-        massBtn[0][0] = (Button) findViewById(R.id.r1c1);
-        massBtn[0][1] = (Button) findViewById(R.id.r1c2);
-        massBtn[0][2] = (Button) findViewById(R.id.r1c3);
-        massBtn[1][0] = (Button) findViewById(R.id.r2c1);
-        massBtn[1][1] = (Button) findViewById(R.id.r2c2);
-        massBtn[1][2] = (Button) findViewById(R.id.r2c3);
-        massBtn[2][0] = (Button) findViewById(R.id.r3c1);
-        massBtn[2][1] = (Button) findViewById(R.id.r3c2);
-        massBtn[2][2] = (Button) findViewById(R.id.r3c3);
+    }
 
-        View.OnClickListener oclBtnOk = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                start.setText("Начать сначала");
-                r1c1.setEnabled(true);
-                r1c2.setEnabled(true);
-                r1c3.setEnabled(true);
-                r2c1.setEnabled(true);
-                r2c2.setEnabled(true);
-                r2c3.setEnabled(true);
-                r3c1.setEnabled(true);
-                r3c2.setEnabled(true);
-                r3c3.setEnabled(true);
-
+    public void onClick(View v)
+    {
+        if (isStarted)
+        {
+            Button vv = (Button) v;
+            if (XO)
+            {
+                vv.setText("X");
+                vv.setEnabled(false);
+                XO =! XO;
             }
-        };
-        start.setOnClickListener(oclBtnOk);
+            else
+            {
+                vv.setText("O");
+                vv.setEnabled(false);
+                XO =! XO;
+            }
+            if (r1c1.getText().toString()== r1c2.getText().toString() && r1c1.getText().toString()==r1c3.getText().toString()
+                    &&r1c1.getText().toString().length()!=0 ||
+                    r1c1.getText().toString() == r2c1.getText().toString() && r1c1.getText().toString() == r3c1.getText().toString()
+                            &&r1c1.getText().toString().length()!=0)
+            {
+                result.setText(r1c1.getText().toString() + "Win");
+                isStarted =! isStarted;
+            }
+            if (r3c3.getText().toString()== r3c2.getText().toString() && r3c3.getText().toString()==r3c1.getText().toString()
+                    &&r3c3.getText().toString().length()!=0 ||
+                    r3c3.getText().toString() == r2c3.getText().toString() && r3c3.getText().toString() == r1c3.getText().toString()
+                            &&r3c3.getText().toString().length()!=0)
+            {
+                result.setText(r3c3.getText().toString() + "Win");
+                isStarted =! isStarted;
+            }
 
+            if (r2c2.getText().toString()== r1c1.getText().toString() && r2c2.getText().toString()==r3c3.getText(git
+                    r2c2.getText().toString() == r1c3.getText().toString() && r2c2.getText().toString() == r3c1.getText().toString()
+                            &&r2c2.getText().toString().length()!=0 ||
+                    r2c2.getText().toString()== r1c2.getText().toString() && r2c2.getText().toString()==r3c2.getText().toString()
+                            &&r2c2.getText().toString().length()!=0 ||
+                    r2c2.getText().toString() == r2c1.getText().toString() && r2c2.getText().toString() == r2c3.getText().toString()
+                            &&r2c2.getText().toString().length()!=0 )
+            {
+                result.setText(r3c3.getText().toString() + "Win");
+                isStarted =! isStarted;
+            }
+        }
+    }
+
+    public void onClickStart (View v)
+    {
+        if (restart)
+        {
+            Button vv = (Button) v;
+            isStarted = true;
+            vv.setText("Restart");
+            restart = false;
+        }
+        else
+        {
+            Intent act = new Intent(this,MainActivity.class);
+            startActivity(act);
+        }
     }
 
 }
