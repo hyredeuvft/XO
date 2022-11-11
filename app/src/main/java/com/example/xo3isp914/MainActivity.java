18ccab2023195066d1be7a41ccaadb7454bbb6e5
Студент <student7@iMac7.local>
2022-11-11 12:11:09 +0300

    First commit

--- app/src/main/java/com/example/xo3isp914/MainActivity.java
+++ app/src/main/java/com/example/xo3isp914/MainActivity.java
@@ -10,119 +10,169 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button r1c1;
-    Button r1c2;
-    Button r1c3;
-    Button r2c1;
-    Button r2c2;
-    Button r2c3;
-    Button r3c1;
-    Button r3c2;
-    Button r3c3;
-    Button start;
-    TextView result;
-
     Boolean XO = true;
-    Boolean isStarted = false;
-    Boolean restart = true;
-    Integer count = 0;
+    Boolean restart = false;
+
+    TextView tvMain;
+
+    Button btn00;
+    Button btn01;
+    Button btn02;
+    Button btn10;
+    Button btn11;
+    Button btn12;
+    Button btn20;
+    Button btn21;
+    Button btn22;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn00 = (Button) findViewById(R.id.button00);
+        btn01 = (Button) findViewById(R.id.button01);
+        btn02 = (Button) findViewById(R.id.button02);
+        btn10 = (Button) findViewById(R.id.button10);
+        btn11 = (Button) findViewById(R.id.button11);
+        btn12 = (Button) findViewById(R.id.button12);
+        btn20 = (Button) findViewById(R.id.button20);
+        btn21 = (Button) findViewById(R.id.button21);
+        btn22 = (Button) findViewById(R.id.button22);
+        tvMain = (TextView) findViewById(R.id.textView2);
+        btn00.setEnabled(false);
+        btn01.setEnabled(false);
+        btn02.setEnabled(false);
+        btn10.setEnabled(false);
+        btn11.setEnabled(false);
+        btn12.setEnabled(false);
+        btn20.setEnabled(false);
+        btn21.setEnabled(false);
+        btn22.setEnabled(false);
 
-        r1c1 = (Button) findViewById(R.id.r1c1);
-        r1c2 = (Button) findViewById(R.id.r1c2);
-        r1c3 = (Button) findViewById(R.id.r1c3);
-        r2c1 = (Button) findViewById(R.id.r2c1);
-        r2c2 = (Button) findViewById(R.id.r2c2);
-        r2c3 = (Button) findViewById(R.id.r2c3);
-        r3c1 = (Button) findViewById(R.id.r3c1);
-        r3c2 = (Button) findViewById(R.id.r3c2);
-        r3c3 = (Button) findViewById(R.id.r3c3);
-        start = (Button) findViewById(R.id.start);
-        result = (TextView) findViewById(R.id.Result);
-        start.setOnClickListener(this::onClickStart);
-        r1c1.setOnClickListener(this::onClick);
-        r1c2.setOnClickListener(this::onClick);
-        r1c3.setOnClickListener(this::onClick);
-        r2c1.setOnClickListener(this::onClick);
-        r2c2.setOnClickListener(this::onClick);
-        r2c3.setOnClickListener(this::onClick);
-        r3c3.setOnClickListener(this::onClick);
-        r3c2.setOnClickListener(this::onClick);
-        r3c1.setOnClickListener(this::onClick);
+
 
     }
-
-    public void onClick(View v)
-    {
-        if (isStarted)
-        {
-            Button vv = (Button) v;
-            if (XO)
-            {
-                vv.setText("X ");
-                vv.setEnabled(false);
-                XO =! XO;
-            }
-            else
-            {
-                vv.setText("O ");
-                vv.setEnabled(false);
-                XO =! XO;
-            }
-            if (r1c1.getText().toString()== r1c2.getText().toString() && r1c1.getText().toString()==r1c3.getText().toString()
-                    &&r1c1.getText().toString().length()!=0 ||
-                    r1c1.getText().toString() == r2c1.getText().toString() && r1c1.getText().toString() == r3c1.getText().toString()
-                            &&r1c1.getText().toString().length()!=0)
-            {
-                result.setText(r1c1.getText().toString() + "Win");
-                isStarted =! isStarted;
-            }
-            if (r3c3.getText().toString()== r3c2.getText().toString() && r3c3.getText().toString()==r3c1.getText().toString()
-                    &&r3c3.getText().toString().length()!=0 ||
-                    r3c3.getText().toString() == r2c3.getText().toString() && r3c3.getText().toString() == r1c3.getText().toString()
-                            &&r3c3.getText().toString().length()!=0)
-            {
-                result.setText(r3c3.getText().toString() + "Win");
-                isStarted =! isStarted;
-            }
-
-            if (r2c2.getText().toString()== r1c1.getText().toString() && r2c2.getText().toString()==r3c3.getText().toString() &&r2c2.getText().toString().length()!=0 ||
-                    r2c2.getText().toString() == r1c3.getText().toString() && r2c2.getText().toString() == r3c1.getText().toString()
-                            &&r2c2.getText().toString().length()!=0 ||
-                    r2c2.getText().toString()== r1c2.getText().toString() && r2c2.getText().toString()==r3c2.getText().toString()
-                            &&r2c2.getText().toString().length()!=0 ||
-                    r2c2.getText().toString() == r2c1.getText().toString() && r2c2.getText().toString() == r2c3.getText().toString()
-                            &&r2c2.getText().toString().length()!=0 )
-            {
-                result.setText(r3c3.getText().toString() + "Win");
-                isStarted =! isStarted;
-            }
-            if (count==8 && result.getText().toString().length()==15)
-            {
-                result.setText("Ничья");
-            }
-            count++;
+    public void onClick(View v) {
+        Button vv =(Button) v;
+        vv.setEnabled(false);
+        if(XO){
+            vv.setText("X");
+            XO=false;
         }
+        else {
+            vv.setText("O");
+            XO=true;
+        }
+        XO();
+        Check();
+
+    }
+    public void Win(String s){
+        if(s.toString()=="X"){
+            tvMain.setText("Выиграл Х");
+            restart = true;
+        }
+        else if (s.toString()=="O"){
+            tvMain.setText("Выиграл О");
+            restart = true;
+        }
+        else{
+            tvMain.setText("Ничья");
+            restart = true;
+        }
+        btn00.setEnabled(false);
+        btn01.setEnabled(false);
+        btn02.setEnabled(false);
+        btn10.setEnabled(false);
+        btn11.setEnabled(false);
+        btn12.setEnabled(false);
+        btn20.setEnabled(false);
+        btn21.setEnabled(false);
+        btn22.setEnabled(false);
+    }
+    public void Check() {
+        if(btn11.getText().toString().length() != 0 && btn00.getText().toString() == btn11.getText().toString() && btn11.getText().toString() == btn22.getText().toString())
+        {
+            Win(btn11.getText().toString());
+            return;
+        }
+        if(btn11.getText().toString().length() != 0 && btn01.getText().toString() == btn11.getText().toString() && btn11.getText().toString() == btn21.getText().toString())
+        {
+            Win(btn11.getText().toString());
+            return;
+        }
+        if(btn11.getText().toString().length() != 0 && btn02.getText().toString() == btn11.getText().toString() && btn11.getText().toString() == btn20.getText().toString())
+        {
+            Win(btn11.getText().toString());
+            return;
+        }
+        if(btn11.getText().toString().length() != 0 && btn10.getText().toString() == btn11.getText().toString() && btn11.getText().toString() == btn12.getText().toString())
+        {
+            Win(btn11.getText().toString());
+            return;
+        }
+        if(btn00.getText().toString().length() != 0 && btn00.getText().toString() == btn01.getText().toString() && btn01.getText().toString() == btn02.getText().toString())
+        {
+            Win(btn00.getText().toString());
+            return;
+        }
+        if(btn00.getText().toString().length() != 0 && btn00.getText().toString() == btn10.getText().toString() && btn10.getText().toString() == btn20.getText().toString())
+        {
+            Win(btn00.getText().toString());
+            return;
+        }
+        if(btn22.getText().toString().length() != 0 && btn22.getText().toString() == btn21.getText().toString() && btn21.getText().toString() == btn20.getText().toString())
+        {
+            Win(btn11.getText().toString());
+            return;
+        }
+        if(btn22.getText().toString().length() != 0 && btn22.getText().toString() == btn12.getText().toString() && btn12.getText().toString() == btn02.getText().toString())
+        {
+            Win(btn11.getText().toString());
+            return;
+        }
+        else if(btn00.getText().toString().length() != 0  && btn01.getText().toString().length() != 0  && btn02.getText().toString().length() != 0  && btn10.getText().toString().length() != 0
+                && btn11.getText().toString().length() != 0 && btn12.getText().toString().length() != 0  && btn20.getText().toString().length() != 0 && btn21.getText().toString().length() != 0
+                && btn22.getText().toString().length() != 0)
+        {
+            Win("");
+        }
+
     }
 
-    public void onClickStart (View v)
-    {
-        if (restart)
-        {
+    public void Start(View v){
+        if (restart){
+            Intent act = new Intent(this, MainActivity.class);
+            startActivity(act);
+
+        }
+        else{
             Button vv = (Button) v;
-            isStarted = true;
-            vv.setText("Restart");
-            restart = false;
+            vv.setText("Начать заново");
+            restart=true;
+            btn00.setEnabled(true);
+            btn01.setEnabled(true);
+            btn02.setEnabled(true);
+            btn10.setEnabled(true);
+            btn11.setEnabled(true);
+            btn12.setEnabled(true);
+            btn20.setEnabled(true);
+            btn21.setEnabled(true);
+            btn22.setEnabled(true);
+
+        }
+        XO();
+    }
+    public void XO()
+    {
+        if (XO)
+        {
+            tvMain.setText("Ход Х");
         }
         else
-        {
-            Intent act = new Intent(this,MainActivity.class);
-            startActivity(act);
+            { tvMain.setText("Ход О");
         }
     }
-
 }
